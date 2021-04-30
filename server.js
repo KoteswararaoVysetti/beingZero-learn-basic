a003b2a1ea3be18a0d9e08cc1255bc489cfdc774
Im_a_Negative1 <vvkoteswararao123@gmail.com>
2021-04-30 10:27:47 +0530

    KK

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
  
 const app = express();
- app.use(express.static(__dirname+"/1stcss.css"));
- app.use(express.static(__dirname+"/1stjs.js"));
+ app.use(express.static(__dirname+"/fend"));
 app.get("/", function(req, res){
     res.send("Welcome");
 })
