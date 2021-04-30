729f6f23f9f754a08a7fb316d9b9b124c09834a1
Im_a_Negative1 <vvkoteswararao123@gmail.com>
2021-04-30 10:23:24 +0530

    hhh

--- 1stjs.js
+++ 1stjs.js
@@ -1,21 +1,3 @@
-const express = require('express');
- 
-const app = express();
- app.use(express.static(__dirname+"/1stcss.css"));
- app.use(express.static(__dirname+"/1stjs.js"));
-app.get("/", function(req, res){
-    res.send("Welcome");
-})
-app.get("/resume", function(req, res){
-    res.sendFile(__dirname+"/index.html");
-})
-// Heroku will automatically set an environment variable called PORT
-const PORT = process.env.PORT || 3000;
- 
-// Start the server
-app.listen(PORT, function(){
-    console.log("Server Starting running on http://localhost:"+PORT);
-})
 
 function primes(){
     document.getElementById('CPP').src='primes.jpg';
